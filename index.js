817996db9610964089d016d0a1f9d15f22a17af8
Nuno Job <nunojobpinto@gmail.com>
2013-07-30 17:51:10 +0100

    foobar

--- index.js
+++ index.js
@@ -5,27 +5,54 @@ var http = require('http')
   ;
 
 var server = http.createServer(function (req, res) {
+  //
+  // create a temp file
+  //
   tmp.file(function _tempFileCreated(err, path) {
+    //
+    // if we have problems creating a simple temp file then its
+    // better not to do anything at all
+    //
     if (err) throw err;
-    
+
+    //
+    // create a stream for writing to our tmp file
+    // 
     var fileStream = fs.createWriteStream(path);
 
     //
-    // data is completely written
+    // data is completely written to tmp file
     //
     fileStream.on('end', function () {
+      //
+      // execute pdflatex on that latex source
+      //
       exec('pdflatex ' + path, function (err, stdout, stderr) {
+        //
+        // if we there are errors respond in plain text
+        // explaining what happened
+        //
         if(err || stderr) {
           res.writeHead(500, { 'Content-Type': 'text/plain' });
           res.end(err && err.message || stderr);
           return;
         }
+
+        //
+        // else just send the output pdf as response
+        //
         fs.createReadStream(path + '.pdf').pipe(res);
       });
     });
 
+    //
+    // pipe the body of the request to our tmp file
+    //
     req.pipe(fileStream);
   });
 });
 
+//
+// start the server
+//
 server.listen(5050);
