4971ff142b085b118cff2f68044b94b62c8b062e
Nuno Job <nunojobpinto@gmail.com>
2013-08-03 15:53:05 +0100

    simpler version

--- .profile.d/check_for_pdflatex.sh
+++ .profile.d/check_for_pdflatex.sh
@@ -3,11 +3,5 @@
 #
 # install pdflatex if it's not available in the container
 #
-if hash pdflatex 2>/dev/null; then
-  echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list
-
-  apt-get upgrade
-  apt-get update
-
-  apt-get install -y texlive-latex-base texlive-latex-extra curl
-fi
+hash pdflatex 2>/dev/null \
+  || { echo >&2 "pdflatex not detected, installing"; apt-get install -y texlive-latex-base texlive-latex-extra curl; }
